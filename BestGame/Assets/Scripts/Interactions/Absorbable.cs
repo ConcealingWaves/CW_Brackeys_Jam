a1296a2edc81b96069a280f7930bba24ae992222
Stoicheia <muscovod@gmail.com>
2021-02-16 00:46:50 +1100

    Added breaking away from absorption (untested)

--- BestGame/Assets/Scripts/Interactions/Absorbable.cs
+++ BestGame/Assets/Scripts/Interactions/Absorbable.cs
@@ -1,13 +1,18 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEditor.SceneManagement;
 using UnityEngine;
 
 public class Absorbable : MonoBehaviour
 {
     private Rigidbody2D rb;
-    private bool absorbed;
+    private bool isAbsorbed;
+    
+    private Sprite originalSprite;
+    private string originalLayer;
+    
     [SerializeField] private SpriteRenderer spriteRenderer;
     [SerializeField] private Sprite absorbedSprite;
     [SerializeField] private Enemy killMe;
@@ -15,6 +20,9 @@ public class Absorbable : MonoBehaviour
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        SetRBValues(rb);
+        originalLayer = LayerMask.LayerToName(gameObject.layer);
+        originalSprite = spriteRenderer.sprite;
     }
 
     public void GetAbsorbed(Absorber absorber)
@@ -23,6 +31,21 @@ public class Absorbable : MonoBehaviour
         Destroy(rb);
         spriteRenderer.sprite = absorbedSprite;
         killMe.enabled = false;
-        absorbed = true;
+        isAbsorbed = true;
+    }
+
+    public void Breakaway()
+    {
+        gameObject.layer = LayerMask.NameToLayer(originalLayer);
+        rb = gameObject.AddComponent<Rigidbody2D>();
+        SetRBValues(rb);
+        spriteRenderer.sprite = originalSprite;
+        killMe.enabled = true;
+        isAbsorbed = false;
+    }
+
+    private void SetRBValues(Rigidbody2D r)
+    {
+        r.gravityScale = 0;
     }
 }
