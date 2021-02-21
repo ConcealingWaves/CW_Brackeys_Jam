02110ed7fccdf58d85c74906da8e9688776a5597
Stoicheia <muscovod@gmail.com>
2021-02-21 19:37:26 +1100

    sound on absorb

--- BestGame/Assets/Scripts/Audio/PlayerAudio.cs
+++ BestGame/Assets/Scripts/Audio/PlayerAudio.cs
@@ -10,25 +10,32 @@ public class PlayerAudio : MonoBehaviour
 {
     [SerializeField] private AudioClip playerDamageClip;
     [SerializeField] private AudioClip playerDeathClip;
+    [SerializeField] private AudioClip absorbClip;
 
     private AudioSource source;
     private HealthHaver player;
+    private Absorbable absorbable;
     private void Awake()
     {
         source = GetComponent<AudioSource>();
         player = GetComponent<HealthHaver>();
+        absorbable = GetComponent<Absorbable>();
     }
 
     private void OnEnable()
     {
         player.OnThisHit += PlayDamageClip;
         player.OnThisDie += PlayDeathClip;
+        if(absorbable!=null)
+            absorbable.OnThisAbsorb += PlayAbsorbClip;
     }
 
     private void OnDisable()
     {
         player.OnThisHit -= PlayDamageClip;
         player.OnThisDie -= PlayDeathClip;
+        if(absorbable!=null)
+            absorbable.OnThisAbsorb -= PlayAbsorbClip;
     }
 
     private void PlayDamageClip(float n, HealthHaver hh)
@@ -41,6 +48,14 @@ public class PlayerAudio : MonoBehaviour
     {
         if(playerDeathClip!=null)
             source.PlayOneShot(playerDeathClip);
+    }
+    
+    private void PlayAbsorbClip()
+    {
+        if (absorbClip != null)
+        {
+            source.PlayOneShot(absorbClip, 0.4f);
+        }
     }
     
 }
